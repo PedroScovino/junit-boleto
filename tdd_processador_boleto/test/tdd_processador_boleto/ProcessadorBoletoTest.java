5436cdecbe97a197e951377a39c9de5e0c3651eb
Pedro Rocha <pedroscovino@id.uff.br>
2020-10-12 18:33:11 -0300

    Teste Final - OK

--- tdd_processador_boleto/test/tdd_processador_boleto/ProcessadorBoletoTest.java
+++ tdd_processador_boleto/test/tdd_processador_boleto/ProcessadorBoletoTest.java
@@ -9,14 +9,17 @@ public class ProcessadorBoletoTest {
 
 	Boleto boleto;
 	Boleto boleto2;
+	Boleto boleto3;
 	ListaBoleto listaBoleto;
 	Fatura fatura;
+	Fatura fatura2;
 	ProcessadorBoleto processador;
 	
 	@BeforeEach
 	public void inicializaBoleto() {
 		boleto = new Boleto(1, "12/10/2020", 225.00);
 		boleto2 = new Boleto(2, "12/10/2020", 500.00);
+		boleto3 = new Boleto(3, "12/10/2020", 500.00);
 	}
 	
 	@BeforeEach
@@ -27,6 +30,7 @@ public class ProcessadorBoletoTest {
 	@BeforeEach
 	public void inicializaFatura() {
 		fatura = new Fatura("Pedro", "12/10/2020", 600.00);	
+		fatura2 = new Fatura("Pedro", "12/10/2020", 2000.00);	
 	}
 
 	@BeforeEach
@@ -67,5 +71,14 @@ public class ProcessadorBoletoTest {
 		listaBoleto.addItem(boleto2);
 		String pagamento = processador.processaPagamento(listaBoleto, fatura);
 		Assertions.assertEquals("PAGO", pagamento);
+	}
+	
+	@Test
+	public void verificaPagamentoFalha() {
+		listaBoleto.addItem(boleto);
+		listaBoleto.addItem(boleto2);
+		listaBoleto.addItem(boleto3);
+		String pagamento = processador.processaPagamento(listaBoleto, fatura2);
+		Assertions.assertEquals("NÃO PAGO", pagamento);
 	}
 }
