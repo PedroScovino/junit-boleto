37e599f585fe6eda20c9efb988721dda6e9486b1
Pedro Rocha <pedroscovino@id.uff.br>
2020-10-12 17:15:59 -0300

    Teste 1 - OK

--- tdd_processador_boleto/test/tdd_processador_boleto/ProcessadorBoletoTest.java
+++ tdd_processador_boleto/test/tdd_processador_boleto/ProcessadorBoletoTest.java
@@ -1,6 +1,9 @@
 package tdd_processador_boleto;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ProcessadorBoletoTest {
 
@@ -9,5 +12,14 @@ public class ProcessadorBoletoTest {
 	@BeforeEach
 	public void inicializaBoleto() {
 		boleto = new Boleto(1, "12/10/2020", 50.00);
+	}
+	
+	@Test
+	public void verificaInformaçõesBoleto() {
+		Assertions.assertAll("boleto",
+				() -> assertEquals(1, boleto.getCodBoleto()),
+				() -> assertEquals("12/10/2020", boleto.getData()),
+				() -> assertEquals(50.00, boleto.getValor())
+				);
 	}
 }
